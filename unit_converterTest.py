c763d18283988d7652979ab4e554384f2a333cec
Tombo <anisimov.dany@mail.ru>
2022-10-09 19:52:51 +0300

    Add tests for volume and mass

--- unit_converterTest.py
+++ unit_converterTest.py
@@ -47,3 +47,24 @@ class ConverterTest(unittest.TestCase):
         self.assertEqual(Converter.convert("Длина", "метр", "аршин", "1"), 1.40607424072)
         self.assertEqual(Converter.convert("Длина", "верста", "метр", "1"), 1066.8)
         self.assertEqual(Converter.convert("Длина", "дюйм (США)", "метр", "1"), 0.0254)
+
+    def test_convert_volume(self):
+        self.assertEqual(Converter.convert("Объём", "гигалитр", "нанолитр", "1"), 1e+18)
+        self.assertEqual(Converter.convert("объём", "миллилитр", "четверик", "100"), 0.00381112085)
+        self.assertEqual(Converter.convert("объЁм", "жидкая унция", "кружка", "12"), 0.27048959966)
+
+    def test_convert_mass(self):
+        self.assertEqual(Converter.convert("Масса", "пуд", "килограмм", "1"), 16.3807)
+        self.assertEqual(Converter.convert("масса", "фунт (сша)", "фунт", "1"), 1.10762720626)
+        self.assertEqual(Converter.convert("маСса", "унция (сша)", "грамм", "12"), 340.1942775)
+
+    def test_convert_wrong_types(self):
+        self.assertEqual(Converter.convert("Масса", "пуд", "литр", "1"), None)
+        self.assertEqual(Converter.convert("объём", "пуд", "литр", "1"), None)
+        self.assertEqual(Converter.convert("длина", "пуд", "литр", "1"), None)
+
+    def test_is_unit_of_wrong_mode(self):
+        self.assertEqual(Converter.is_unit_of_mode("ДЛина", "метр"), True)
+        self.assertEqual(Converter.is_unit_of_mode("ДЛина", "литр"), False)
+        self.assertEqual(Converter.is_unit_of_mode("ДЛина", "грамм"), False)
+
